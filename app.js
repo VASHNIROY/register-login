6bab1e7524d05e2380e2a4aa27a6e7447ebf7d02
VASHNIROY <96440335+VASHNIROY@users.noreply.github.com>
2022-12-14 18:15:53 +0530

    Update app.js
    changed few

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const app = express();
 
+const bcrypt = require("bcrypt");
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
-app.use(express.json());
 const path = require("path");
+const app = express();
+app.use(express.json());
+
 const databasePath = path.join(__dirname, "userData.db");
 
 let database = null;
@@ -35,15 +37,14 @@ app.post("/register", async (request, response) => {
 
   if (userDetails !== undefined) {
     //user already exists
-    response.status = 400;
+    response.status(400);
     response.send("User already exists");
   } else {
     if (password < 5) {
-      response.status = 400;
+      response.status(400);
       response.send("Password is too short");
     } else {
-      const createUserQuery;
-      `INSERT INTO 
+      const createUserQuery = `INSERT INTO 
                 user(username,name,password,gender,location)
                 VALUES(
                     '${username}',
@@ -53,7 +54,7 @@ app.post("/register", async (request, response) => {
                     '${location}');`;
       const createdUser = await database.run(createUserQuery);
 
-      response.status = 200;
+      response.status(200);
       response.send("User created successfully");
     }
   }
@@ -68,7 +69,7 @@ app.post("/login", async (request, response) => {
 
   if (userDetails === undefined) {
     //unregistered user
-    response.status = 400;
+    response.status(400);
     response.send("Invalid user");
   } else {
     const isPasswordMatched = await bcrypt.compare(
@@ -76,8 +77,11 @@ app.post("/login", async (request, response) => {
       userDetails.password
     );
     if (isPasswordMatched === true) {
-      response.status = 200;
+      response.status(200);
       response.send("Login success!");
+    } else {
+      response.status(400);
+      response.send("Invalid password");
     }
   }
 });
@@ -86,7 +90,10 @@ app.post("/login", async (request, response) => {
 
 app.put("/change-password", async (request, response) => {
   const { username, oldPassword, newPassword } = request.body;
-
+  const convertnewToHashedPassword = await bcrypt.hash(
+    request.body.newPassword,
+    10
+  );
   const getPasswordQuery = `SELECT *
              FROM user
              WHERE username = '${username}';`;
@@ -97,20 +104,20 @@ app.put("/change-password", async (request, response) => {
   );
 
   if (isPasswordMatched !== true) {
-    response.status = 400;
+    response.status(400);
     response.send("Invalid current password");
   } else {
     if (newPassword < 5) {
-      response.status = 400;
+      response.status(400);
       response.send("Password is too short");
     } else {
       const updatePasswordQuery = `UPDATE 
                         user
                      SET
-                        password = '${newPassword}'
+                        password = '${convertnewToHashedPassword}'
                     WHERE username = '${username}';`;
       await database.run(updatePasswordQuery);
-      response.status = 200;
+      response.status(200);
       response.send("Password updated");
     }
   }
